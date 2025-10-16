fca6b9752ef049241f83c1686ff180e1d2f086c4
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2025-10-15 19:21:27 -0700

    WIP Robot 1 TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SensorUpdateThread.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SensorUpdateThread extends Thread {
+
+    private final RobotBase robot;
+
+    public static int THREAD_SLEEP_TIME = 10 ;
+    public SensorUpdateThread(RobotBase robot) {
+        this.robot = robot;
+    }
+    private ElapsedTime elapsedTime;
+    @SuppressWarnings({"InfiniteLoopStatement", "BusyWait"})
+    @Override
+    public void run() {
+//        elapsedTime = new ElapsedTime();
+        try {
+            while(true) {
+                Thread.sleep(THREAD_SLEEP_TIME);
+//                Log.i("loop time: ", ""+elapsedTime.milliseconds());
+//                elapsedTime.reset();
+            }
+        } catch (InterruptedException e) {
+            Log.e("RCActivity", "SensorUpdateThread interrupted");
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/V1RobotTeleOp.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.RobotFactory;
+import org.firstinspires.ftc.teamcode.drive.SensorUpdateThread;
+import org.firstinspires.ftc.teamcode.drive.robot1.Robot1;
+import org.firstinspires.ftc.teamcode.util.GamePad;
+
+@TeleOp(name = "V1 TeleOp")
+@Config
+public class V1RobotTeleOp extends LinearOpMode {
+
+    public MultipleTelemetry multipleTelemetry;
+    public ElapsedTime elapsedTime = new ElapsedTime();
+
+    private Robot1 robot;
+
+    private GamePad gp1, gp2;
+
+    private SensorUpdateThread sensorUpdateThread;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = (Robot1) RobotFactory.createRobot(hardwareMap);
+        robot.teleOpInit();
+
+        gp1 = new GamePad(gamepad1);
+        gp2 = new GamePad(gamepad2);
+
+        waitForStart();
+
+        sensorUpdateThread = new SensorUpdateThread(robot);
+        sensorUpdateThread.start();
+
+        while (opModeIsActive()){
+            driveRobot();
+
+            elapsedTime.reset();
+
+            update();
+        }
+    }
+
+    private void update(){
+        gp1.update();
+        gp2.update();
+        updateTelemetry();
+        robot.updateEverything();
+    }
+
+    private void updateTelemetry(){
+        telemetry.update();
+    }
+
+    private void driveRobot() {
+        double x = 0, y = 0, a = 0;
+        if (gp1.dpadLeft() || gp1.dpadRight()) {
+            a = 0.5 * (gp1.dpadLeft() ? 1 : -1);
+        } else if (gp1.dpadUp() || gp1.dpadDown()) {
+            x = 0.3 * (gp1.dpadUp() ? 1 : -1);
+        } else {
+            x = -gp1.leftStickY();
+            y = -gp1.leftStickX();
+            a = -gp1.rightStickX() * 0.9;
+        }
+        double pow = Math.sqrt(x * x + y * y);
+//        double limPow = driveLim.calculate(pow);
+//        if (pow > MIN_DRIVE_POW) {
+//            x *= limPow / pow;
+//            y *= limPow / pow;
+//        }
+//        a = turnLim.calculate(a);
+//        telemetry.addData("driving", x + " " + y + " " + a);
+        //robot.setDrivePowers(new PoseVelocity2d(new Vector2d(x, y), a));
+    }
+}
