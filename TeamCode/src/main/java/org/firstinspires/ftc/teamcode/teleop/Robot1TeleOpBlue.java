3504285c4553e5bdc79785a6f0909179eb3c67f3
Ethan Lee <ethan7sky@gmail.com>
2025-11-01 18:54:37 -0700

    added teleop with driver 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot1TeleOpBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot1TeleOpBlue.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Blue \uD83D\uDD35 Robot 1 TeleOp", group = "Competition")
-public class Robot1TeleOpBlue extends Robot1TeleOp{
+public class Robot1TeleOpBlue extends Robot1TeleOpForNeel {
     @Override
     protected boolean isRed() {
         return false;
