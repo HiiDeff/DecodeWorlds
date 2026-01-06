72899886f5403bf7eb3d6cb0bc6a32a7f859305a
Calvin <calvinzh2008@gmail.com>
2026-01-06 15:17:56 -0800

    beambreaktest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/BeamBreakTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.Timer;
+
+@Config
+@TeleOp(name="BeamBreak Test", group="Test")
+public class BeamBreakTest extends LinearOpMode {
+    public static double POS = 0.0;
+    public static double MOTORPOWER = 0;
+
+    public MultipleTelemetry multipleTelemetry;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        multipleTelemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+
+        DigitalChannel breakbeamoutput = hardwareMap.get(DigitalChannel.class, "breakbeamoutput");
+        breakbeamoutput.setMode(DigitalChannel.Mode.OUTPUT);
+        DigitalChannel breakbeamreceiver = hardwareMap.get(DigitalChannel.class, "breakbeamreceiver");
+        breakbeamreceiver.setMode(DigitalChannel.Mode.INPUT);
+
+        breakbeamoutput.setState(true);
+
+        boolean prevState = true;
+        boolean currState = true;
+        int count = 0;
+        long prevTime = 0;
+        long currTime = 0;
+        boolean empty = true;
+        waitForStart();
+        while (opModeIsActive()) {
+            motor.setPower(MOTORPOWER);
+            currState = breakbeamreceiver.getState();
+            currTime = System.currentTimeMillis();
+            if (!currState)
+            {
+                empty = false;
+            }
+            if (currState != prevState)
+            {
+                prevTime = currTime;
+            }
+            else if (currState && (currTime - prevTime > 100)){
+                empty = true;
+            }
+
+            if (!currState && prevState)
+            {
+                count++;
+            }
+            multipleTelemetry.addData("receiver", currState);
+            multipleTelemetry.addData("empty", empty);
+            multipleTelemetry.addData("count", count);
+            multipleTelemetry.addData("output", breakbeamoutput.getState());
+            multipleTelemetry.update();
+            prevState = currState;
+        }
+    }
+}
